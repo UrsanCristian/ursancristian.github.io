d03cf7448c9ce6e92d133017a2fddd386e79a326
UrsanCristian <147237990+UrsanCristian@users.noreply.github.com>
2024-01-08 17:43:46 +0200

    Added download Resume function and text correction

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function openURL(url) {
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+function downloadResume() {
+    var link = document.createElement('a');
+    link.href = './assets/CV-UrsanCristian.pdf'; 
+    link.download = 'CV-UrsanCristian.pdf'; 
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  }
